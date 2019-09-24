781cfe630f2e3dde3ef20d752969243be454aec3
Fabio Falzoi <fabio.falzoi84@gmail.com>
2019-09-24 13:07:58 +0200

    Improve profiling and tracing example

--- 04-profiling-and-tracing/profilingexamples/wholeprogram/main.go
+++ 04-profiling-and-tracing/profilingexamples/wholeprogram/main.go
@@ -11,10 +11,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	_ "net/http/pprof"
 	"os"
+	"runtime"
 	"runtime/pprof"
+	"runtime/trace"
 )
 
 // Fibonacci returns the n-th element of the Fibonacci sequence
@@ -27,17 +28,59 @@ func Fibonacci(n int) int {
 }
 
 func main() {
+	// add profiling and tracing flags
+	traceFile := flag.String("trace", "", "write program trace to file")
 	cpuProfile := flag.String("cpuprofile", "", "write cpu profile to file")
+	memProfile := flag.String("memprofile", "", "write heap profile to file")
 	flag.Parse()
 
+	// start tracing program events to file
+	if *traceFile != "" {
+		f, err := os.Create(*traceFile)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		if err := trace.Start(f); err != nil {
+			panic(err)
+		}
+		defer trace.Stop()
+	}
+
+	// start CPU profiling and save it to file
 	if *cpuProfile != "" {
 		f, err := os.Create(*cpuProfile)
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
-		pprof.StartCPUProfile(f)
+
+		if err := pprof.StartCPUProfile(f); err != nil {
+			panic(err)
+		}
 		defer pprof.StopCPUProfile()
 	}
 
-	fmt.Println(Fibonacci(100))
+	// set memory allocations sampling rate to the maximum
+	// as soon as we are interested to profile allocations
+	runtime.MemProfileRate = 1
+
+	// code to profile
+	fmt.Println(Fibonacci(42))
+
+	// save memory allocations profile to file
+	if *memProfile != "" {
+		f, err := os.Create(*memProfile)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		// get up-to-date statistics
+		runtime.GC()
+
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			panic(err)
+		}
+	}
 }
